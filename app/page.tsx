f88e57860f036c872e53ac2d8983a3fc0ceff8bd
Mory004 <konebamory2004@gmail.com>
2026-04-23 12:52:47 +0000

    modif page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         Salut Je suis un nouveau sur Next.js!
       </h1>
       <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
-        Je viens darriver.
+        Je viens darriver merci.
       </p>
       <Image
         src="/nextjs-logo.png"
